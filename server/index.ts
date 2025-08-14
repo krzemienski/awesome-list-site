c243a24df94fd2a37e40dacb636220910214b9be
Nick Krzemienski <krzemienski@gmail.com>
2025-08-14 00:41:07 +0000

    All origins

--- server/index.ts
+++ server/index.ts
@@ -4,11 +4,11 @@ import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 
-// CORS middleware
+// CORS middleware - accept all origins
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control');
   
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
